0510e6747fba5ee056392613bef8c081f6322dc5
MalikShoaibAhmadKhan <mshoaibak10@gmail.com>
2025-03-25 01:55:58 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
-echo "Running script from Jenkins!" > output.txt
-echo "====================================" >> output.txt
-
-# Display current date and time
-echo "Current date and time: $(date)" >> output.txt
-echo "====================================" >> output.txt
-
-# Fetch system disk usage
-echo "Disk Usage:" >> output.txt
-df -h >> output.txt
-echo "====================================" >> output.txt
-
-# Display running processes
-echo "Running Processes:" >> output.txt
-ps aux >> output.txt
-echo "====================================" >> output.txt
-
-echo "Script execution completed!" >> output.txt
+echo "Generating system info..." > system_info.txt
+date >> system_info.txt
+uname -a >> system_info.txt
+df -h >> system_info.txt
+ps aux >> system_info.txt
+echo "System Info Generated Successfully."
